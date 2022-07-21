24abf41120b158fbf7e2f08f2a2e97b419699439
Lucky-victory <blizwonder25@gmail.com>
2022-07-21 06:13:02 +0100

    added components, updated maintenance time

--- maintenance/main.js
+++ maintenance/main.js
@@ -7,7 +7,7 @@ const timerMinutes = document.querySelector("[data-timer-minutes]");
 const timerHours = document.querySelector("[data-timer-hours]");
 const timerDays = document.querySelector("[data-timer-days]");
 const circleTotalLength = 380;
-const startDate = "2022-07-20T00:00:00";
+const startDate = "2022-08-18T00:00:00";
 const startDay = new Date(startDate).getTime();
 const intervalId = setInterval(() => {
   const today = new Date().getTime();
--- v2/portfolio/src/app/app.component.ts
+++ v2/portfolio/src/app/app.component.ts
@@ -1,10 +1,26 @@
 import { Component } from '@angular/core';
+import { IGestureEvent } from './interfaces/gesture.interface';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'portfolio';
+
+  gestureStart(ev: IGestureEvent) {
+    console.log('touched', ev);
+  }
+  gestureMove(ev: IGestureEvent) {
+    const target = ev.elem.nativeElement as HTMLDivElement;
+    target.style.transform = `rotate(${ev.event.deltaY / 2}deg) translateX(${
+      ev.event.deltaX
+    }px)`;
+    target.style.boxShadow = '5px 8px 8px #fcd';
+  }
+  gestureEnd(ev: IGestureEvent) {
+    const target = ev.elem.nativeElement as HTMLDivElement;
+    target.style.transform = '';
+  }
 }
